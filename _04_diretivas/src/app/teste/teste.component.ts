01bca91aa4ff48e2a260f2d6e6cc658f27be6496
Felipe Leme <lemefe21@gmail.com>
2016-11-20 20:29:18 -0200

    Realizando build e geração de componente teste para o projeto

--- _04_diretivas/src/app/teste/teste.component.ts
+++ _04_diretivas/src/app/teste/teste.component.ts
@@ -3,12 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-teste',
   templateUrl: './teste.component.html',
-  styleUrls: ['./teste.component.scss']
+  styleUrls: ['./teste.component.css']
 })
 export class TesteComponent implements OnInit {
-
-  //componente criado alterando na geração o tipo de pré-processador do .css
-  //ng set defaults.styleExt scss
 
   constructor() { }
 
